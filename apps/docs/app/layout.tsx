810e8d2780b9a9065555abd172e9c94785aadd06
harkirat <harkirat96@gmail.com>
2024-12-31 15:09:52 +0530

    Added dockerfile

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>): any {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,9 +1,5 @@
 "use client";
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css"
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import React from "react";
 import { useState } from "react";
 
 
@@ -12,6 +8,8 @@ export default function Home() {
   const [value, setValue] = useState('');
 
   return (
-    <ReactQuill theme="snow" value={value} onChange={setValue} />
+    <div>
+      Website
+    </div>
   );
 }
